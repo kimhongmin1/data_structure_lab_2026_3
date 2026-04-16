2ae0c63f27ae629e8cac09eb2c6ad42752f4e6fa
kimhongmin1 <ytxis1@naver.com>
2026-04-16 14:42:34 +0900

    stack

---
+++ LEC04_StudentClass/LEC04_StudentClass/Student.cpp
@@ -0,0 +1,13 @@
+﻿#include "StudentStack.h"
+
+int main() {
+	StudentStack stack;
+	stack.push(student(20230001, "홍길동", "컴퓨터공학과"));
+	stack.push(student(20230002, "김철수", "전자공학과"));
+	stack.push(student(20230003, "이영희", "기계공학과"));
+	stack.push(student(20230004, "박민수", "화학공학과"));
+	stack.display();
+	stack.pop();
+	stack.display();
+	return 0;
+}
---
+++ LEC04_StudentClass/LEC04_StudentClass/StudentStack.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <cstring>
+
+const int MAX_STRING = 50;
+const int MAX_STACK_SIZE = 20;
+
+class student {
+	int id;
+	char name[MAX_STRING];
+	char dept[MAX_STRING];
+public:
+	student(int i = 0, const char* n = "", const char* d = "") {
+		set(i, n, d);
+	}
+	void set(int i, const char* n, const char* d) {
+		id = i;
+		strncpy(name, n, MAX_STRING - 1);
+		name[MAX_STRING - 1] = '\0';
+		strncpy(dept, d, MAX_STRING - 1);
+		dept[MAX_STRING - 1] = '\0';
+	}
+	void display() const {
+		printf("학번: %d, 이름: %s, 학과: %s\n", id, name, dept);
+	}
+};
+
+class StudentStack {
+	int top;
+	student data[MAX_STACK_SIZE];
+public:
+	StudentStack() : top(-1) {}
+
+	bool isEmpty() const {
+		return top == -1;
+	}
+	bool isFull() const {
+		return top == MAX_STACK_SIZE - 1;
+	}
+	void push(const student& s) {
+		if (isFull()) {
+			printf("스택이 가득 찼습니다.\n");
+			return;
+		}
+		data[++top] = s;
+	}
+	student pop() {
+		if (isEmpty()) {
+			printf("스택이 비어 있습니다.\n");
+			return student();
+		}
+		return data[top--];
+	}
+	void display() const {
+		printf("[스택 항목의 수 = %2d]\n", top + 1);
+		for (int i = 0; i <= top; i++) {
+			data[i].display();
+		}
+		printf("\n");
+	}
+};
